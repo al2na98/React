98c7844560301366511fa4e2af7c58e4d64f1cca
Alena <dudoladovaal75@gmail.com>
2025-10-30 17:56:30 +0700

    add decrement, increment

--- react-start/src/App.jsx
+++ react-start/src/App.jsx
@@ -1,25 +1,47 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      count: 0
+    constructor() {
+        super();
+        this.state = {
+            count: 0,
+        };
     };
-  }
-    
-    
-    render() {
-      return (
-        <div className="App">
-        Hello
- {/* использование стрелочной ф-и внутри (для небольших изменений) */}
-        <button onClick={() => this.setState({count: this.state.count + 1})}>{this.state.count}</button>
-      </div>
-    );
-  }
-}
 
+    decrement = () => {
+      this.setState({ count: this.state.count - 1 });
+      
+    };
+    
+    increment = () => {
+      this.setState({ count: this.state.count + 1 });
+    };
+
+    reset = () => {
+      this.setState({ count: this.state.count = 0 });
+
+    }
+
+    render() {
+        return (
+            <div className="App" style={{margin:'auto', width: '300px'}}>
+                <button 
+                onClick={this.decrement}>
+                  -
+                </button>
+                <span 
+                style={{margin: '0 0.5rem', display: 'inline-block'}}>
+                  {this.state.count}
+                </span>
+                <button 
+                onClick={this.increment}>+</button>
+                <br />
+                <button 
+                onClick={this.reset}
+                >reset</button>
+            </div>
+        );
+    }
+}
 
 export default App;
