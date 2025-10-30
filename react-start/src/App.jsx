f35c45249d1f4f5397095c39ff14ba3e660085ac
Alena <dudoladovaal75@gmail.com>
2025-10-30 17:40:35 +0700

    using arrow func inside tag

--- react-start/src/App.jsx
+++ react-start/src/App.jsx
@@ -3,26 +3,19 @@ import React, {Component} from "react";
 class App extends Component {
   constructor() {
     super();
-    
+
     this.state = {
       count: 0
     };
-
-    this.handleClick = this.handleClick.bind(this);
   }
     
-    //метод
-    handleClick(){
-      
-      this.setState({count: this.state.count + 1})
-    }
     
     render() {
       return (
         <div className="App">
         Hello
-
-        <button onClick={this.handleClick}>{this.state.count}</button>
+ {/* использование стрелочной ф-и внутри (для небольших изменений) */}
+        <button onClick={() => this.setState({count: this.state.count + 1})}>{this.state.count}</button>
       </div>
     );
   }
